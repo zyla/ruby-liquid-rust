1358b558af97e67fdcb97f280ddd46754c1fa941
Maciej Bielecki <zyla@prati.pl>
2020-03-20 15:03:17 +0100

    Very hacky solution with wrapping Ruby objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 #[macro_use] extern crate rutie;
 #[macro_use] extern crate lazy_static;
 extern crate liquid;
+extern crate kstring;
 
 use rutie::{Object, RString, VM, AnyObject, Module, Hash, Fixnum};
+use liquid::model::{ObjectView, ValueView};
+use liquid::model::value::{DisplayCow, ValueCow};
+use liquid::model::scalar::{ScalarCow};
+use kstring::KStringCow;
+use std::iter::empty;
+use std::fmt;
 
 pub struct Template {
     source: String,
@@ -43,23 +50,90 @@ methods!(
         RString::new_utf8(&obj.source)
     }
 
-    fn pub_render(ruby_globals: Hash) -> RString {
+    fn pub_render(ruby_globals: AnyObject) -> RString {
         let obj = _itself.get_data(&*TEMPLATE_WRAPPER);
 
-        let mut globals = liquid::model::Object::new();
-        ruby_globals.unwrap().each(|key, value| {
-            if let Ok(key) = key.try_convert_to::<RString>() {
-                if let Ok(value) = value.try_convert_to::<Fixnum>() {
-                    globals.insert(key.to_string().into(), liquid::model::to_value(&value.to_i64()).unwrap());
-                }
-            }
-        });
-
-        let result = obj.template.render(&globals).unwrap();
+        let result = obj.template.render(&RubyObject(ruby_globals.unwrap())).unwrap();
 
         RString::new_utf8(&result)
     }
 );
+
+#[derive(Debug)]
+struct RubyObject(AnyObject);
+
+impl ValueView for RubyObject {
+    fn as_debug(&self) -> &dyn fmt::Debug {
+        self
+    }
+
+    fn render(&self) -> DisplayCow<'_> {
+        unimplemented!()
+    }
+    fn source(&self) -> DisplayCow<'_> {
+        DisplayCow::Owned(Box::new("<source>"))
+    }
+    fn type_name(&self) -> &'static str {
+        "object"
+    }
+    fn query_state(&self, state: liquid::model::State) -> bool {
+        unimplemented!()
+    }
+
+    fn to_kstr(&self) -> KStringCow<'_> {
+        unimplemented!()
+    }
+    fn to_value(&self) -> liquid::model::Value {
+        unimplemented!()
+    }
+
+    fn as_object(&self) -> Option<&dyn ObjectView> {
+        Some(self)
+    }
+
+    fn as_scalar(&self) -> Option<ScalarCow<'_>> {
+        if let Ok(value) = self.0.try_convert_to::<Fixnum>() {
+            Some(liquid::model::scalar::to_scalar(&value.to_i64()).unwrap())
+        } else {
+            None
+        }
+    }
+}
+
+impl ObjectView for RubyObject {
+    fn as_value(&self) -> &dyn ValueView {
+        self
+    }
+
+    fn size(&self) -> i32 {
+        0
+    }
+
+    fn keys<'k>(&'k self) -> Box<dyn Iterator<Item = KStringCow<'k>> + 'k> {
+        println!("keys");
+        Box::new(empty())
+    }
+
+    fn values<'k>(&'k self) -> Box<dyn Iterator<Item = &'k dyn ValueView> + 'k> {
+        Box::new(empty())
+    }
+
+    fn iter<'k>(&'k self) -> Box<dyn Iterator<Item = (KStringCow<'k>, &'k dyn ValueView)> + 'k> {
+        println!("iter");
+        Box::new(empty())
+    }
+
+    fn contains_key(&self, index: &str) -> bool {
+        let result = self.0.protect_send("has_key?", &[RString::new_utf8(index).to_any_object()]).unwrap().is_true();
+        println!("contains_key({}) -> {}", index, result);
+        result
+    }
+
+    fn get<'k>(&'k self, index: &str) -> Option<&'k dyn ValueView> {
+        println!("get {}", index);
+        Some(Box::leak(Box::new(RubyObject(self.0.protect_send("[]", &[RString::new_utf8(index).to_any_object()]).unwrap()))))
+    }
+}
 
 #[allow(non_snake_case)]
 #[no_mangle]
